0d7997d0eef5191f01eeb41a786d3f0c3e5b4b35
Marc Thiery <thierymarc@hotmail.com>
2019-03-05 00:36:57 +0100

    Add PureCompo, PureCompoProps and StateCompo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {PureCompo, PureCompoProps} from './components/PureCompo';
+import StateCompo from './components/StateCompo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <PureCompo/>
+          <PureCompoProps label={'Marc'} age={21}/>
+          <StateCompo name={'Marc'}/>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
